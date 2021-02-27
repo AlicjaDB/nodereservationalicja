5ac6e7ed5250f10426ace92a0a2691b17c5bf36f
AlicjaDB <fiboza2@gmail.com>
2021-02-27 13:53:51 +0100

    kurs_node

---
+++ functions.js
@@ -0,0 +1,13 @@
+const helloWorld = () => {console.log("Hello World")}
+helloWorld()
+const someText = "jakiś tekt";
+const someFunction = () => { return 2+3 }
+const add = (a,b) => {
+    console.log(a+b)
+}
+module.exports = {
+    helloWorld,
+    add,
+    someText,
+    someFunction
+}
